9055a48e2c829bdf7de05817058ccddb4a291918
TheRValtr <tech.acu678@gmail.com>
2026-07-09 20:41:09 -0400

    Menu basic important template in C

---
+++ C templates/menu.c
@@ -0,0 +1,13 @@
+do {
+    printMenu();
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            // action
+            break;
+        case 0:
+            // exit
+            break;
+    }
+} while (choice != 0);
