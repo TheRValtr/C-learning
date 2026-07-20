eb25e4bbfcff1165df53d06d40dff888216ae293
TheRValtr <tech.acu678@gmail.com>
2026-07-19 20:20:36 -0400

    Basic arithmetic in C vs Assembly

---
+++ Basic C tools and its version in assembly/Basic arithmetic in C.c
@@ -0,0 +1,11 @@
+int main(void)
+{
+    int x = 10;
+
+    x = x + 5;
+    x = x - 2;
+    x++;
+    x--;
+
+    return x;
+}
---
+++ Basic C tools and its version in assembly/basic arithmetic in assembly.asm
@@ -0,0 +1,9 @@
+main:
+    mov eax, 10
+
+    add eax, 5
+    sub eax, 2
+    inc eax
+    dec eax
+
+    ret
